cd329f597fb63cd005a25697c31a94824f8e5e96
Harshitha.C-17 <harshchinnaswamy1706@gmail.com>
2025-11-26 09:52:11 +0530

    commit

---
+++ day4/day 5/pointer5.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int value = 10;
+    int *p = &value;
+    int **pp = &p;
+
+    printf("value = %d\n", value);
+    printf("*p = %d\n", *p);
+    printf("**pp = %d\n",**pp);
+
+    **pp = 99;
+
+    printf("Updated value = %d\n", value);
+
+    return 0;
+}
